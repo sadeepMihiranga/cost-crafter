d52b5fd82b008fffa5f521cdadb70a0d40c5daa4
Imzar Ahamed <55689405+imzarahamed@users.noreply.github.com>
2024-01-21 21:24:21 +0530

    Fix initial query version

--- src/main/java/com/cost/crafter/dal/GenerateReportRepository.java
+++ src/main/java/com/cost/crafter/dal/GenerateReportRepository.java
@@ -16,13 +16,14 @@ public class GenerateReportRepository extends BaseRepository {
 
     public List<GenerateReport> fetchMonthlyReportData(Integer userId, String userInputMonth) throws Exception {
         try {
-            final String readQuery = "SELECT expenses_categories.name, user_budget.budget_amount, SUM(transaction.transaction_amount) AS actual_expense " +
-                    "FROM expenses_categories " +
-                    "JOIN user_budget ON expenses_categories.category_id = user_budget.expenses_category_id " +
+            final String readQuery = "SELECT user_expenses_categories.name, user_budget.budget_amount, SUM(transaction.transaction_amount) AS actual_expense " +
+                    "FROM user_expenses_categories " +
+                    "JOIN user_budget ON user_expenses_categories.expenses_category_id = user_budget.expenses_category_id " +
                     "LEFT JOIN transaction ON user_budget.user_id = transaction.user_id AND user_budget.expenses_category_id = transaction.expenses_category_id " +
-                    "WHERE user_budget.month = ? " +
-                    "GROUP BY expenses_categories.category_id";
-            List<GenerateReport> monthlyReportDataList = read(readQuery, new GenerateReportMapper(), userInputMonth);
+                    "WHERE user_budget.month = ? AND transaction.user_id = ? " +
+                    "GROUP BY user_expenses_categories.expenses_category_id";
+
+            List<GenerateReport> monthlyReportDataList = read(readQuery, new GenerateReportMapper(), userInputMonth, userId);
             return monthlyReportDataList;
         } catch (SQLException e) {
             e.printStackTrace();
